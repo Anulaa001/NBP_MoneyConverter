ac0e9851fd6a47d30eb2207e6d7debfbe1f535fe
Anulaa001 <ania.czupryn@gmail.com>
2021-05-23 11:08:49 +0200

    v1

--- app/src/main/java/com/example/nbpapiconvert/MainActivity.java
+++ app/src/main/java/com/example/nbpapiconvert/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     suma.setText("1");
                 }
                 float f = (float) (Float.parseFloat(suma.getText().toString()) / srednia);
-                rezultat.setText(String.valueOf("Result: " +String.format("%.2f",f)+ " " + wybranaWaluta));
+                rezultat.setText(String.valueOf("Rezultat: " +String.format("%.2f",f)+ " " + wybranaWaluta));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
